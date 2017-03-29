d99c015436b774a952315b1786342dd4ced8aafd
portalmaster12 <classifedethan@gmail.com>
2017-03-29 13:21:11 +0000

    reader

--- motor_pump/motor_pump.ino
+++ motor_pump/motor_pump.ino
@@ -1,19 +1,34 @@
-const int switchPin =2
-const int motorPin =9
-int switchstate =0
+const int switchPin =2;
+const int motorPin =9;
+int switchstate = 0;
+const int Input = 12;
+int moistoure = 0;
+//int ByteRecieved;
 
 void setup() {
   pinMode(motorPin, OUTPUT);
-  pinMode(switchpin, INPUT);
-}
+  pinMode(switchPin, INPUT);
+  pinMode (Input, INPUT);
+//  Serial.begin(9600);
+//  Serial.println(digitalRead(Input));
+} 
 
-void loop() {
-  switchstate = digitalRead(switchPin);
-  
-  if (switchstate == HIGH) {
+void loop(){
+moistoure = digitalRead(Input);
+switchstate = digitalRead(switchPin);
+ 
+  if (moistoure >85) {
     digitalWrite(motorPin, HIGH);
   }
   else{
-    digitalWrite(motorPin,LOW)
+    digitalWrite(motorPin,LOW);
   }
+
+  
+//  if(Serial.available()>0){
+    //ByteRecieved = Serial.read();
+//    Serial.println(moistoure);
+//  }
+ 
 }
+
